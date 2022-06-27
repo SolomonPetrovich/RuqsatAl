a3b4c09714ec53f42bea864b71ded3971f9d081b
Mukhammed-Ali <mukhammedali.zholdasbay@gmail.com>
2022-06-27 18:41:18 +0600

    Fix pass

--- RuqsatAl/security.py
+++
@@ -1,2 +0,0 @@
-C_EMAIL_HOST_PASSWORD = 'txmpmgrgsikuunws'
-C_SECRET_KEY = 'django-insecure-1i$-y%&vc((ivyd!)&v^4%)*8db(sgn=f%dnajqg@zqwe(*tb3'
--- RuqsatAl/settings.py
+++ RuqsatAl/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = C_SECRET_KEY
+SECRET_KEY = 'django-insecure-1i$-y%&vc((ivyd!)&v^4%)*8db(sgn=f%dnajqg@zqwe(*tb3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -170,7 +170,7 @@ LOGOUT_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'solizholdasbai@gmail.com'
-EMAIL_HOST_PASSWORD = C_EMAIL_HOST_PASSWORD
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False

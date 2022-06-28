e530f55f72824636f827430c2cff12ec52f61e75
Mukhammed-Ali <mukhammedali.zholdasbay@gmail.com>
2022-06-29 01:01:29 +0600

    removed email addres

--- RuqsatAl/settings.py
+++ RuqsatAl/settings.py
@@ -169,7 +169,7 @@ LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'solizholdasbai@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
